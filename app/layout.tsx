47a206ee002e2bffe6f5708aca6d9e80af93c33d
lisgeh2 <lisa.gehrig2@uzh.ch>
2026-02-26 19:00:40 +0100

    little CSS Layout Playarounds :)

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Student XX-XXX-XXX",
+  title: "Student Lisa",
   description: "sopra-fs26-template-client",
 };
 
