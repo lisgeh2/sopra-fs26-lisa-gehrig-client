dea4231f54d7430b1eb954dd63ae9a8dbf584513
lisgeh2 <lisa.gehrig2@uzh.ch>
2026-02-28 12:44:41 +0100

    pretty Status

--- app/types/user.ts
+++ app/types/user.ts
@@ -1,9 +1,11 @@
+import { UserStatus } from "@/types/status";
+
 export interface User {
   id: number | null;
   name: string | null;
   username: string | null;
   token: string | null;
-  status: string | null;
+  status: UserStatus;
   bio?: string | null;
   creationDate?: string | null;
 }
